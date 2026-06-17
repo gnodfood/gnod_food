3165361fae64058598eb8a22d45043cd0a5f543b
gnodfood <hoanghaopq2005@gmail.com>
2026-06-17 11:02:39 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/gnod_food/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
